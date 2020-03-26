cf63370bc81e4e321d7b58bc25eec051c0c646f4
Leonardo Setubal <setubal43@gmail.com>
2020-03-25 23:01:42 -0300

    global: Configurando projeto

---
+++ src/pages/login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css'
+import heroesImage from '../../assets/heroes.png'
+
+export default function Login(props) {
+    return (
+        <div className="login-container">
+            <section className="form"></section>
+        </div>
+    );
+}
