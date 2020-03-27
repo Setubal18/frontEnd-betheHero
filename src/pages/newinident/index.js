259acfe48e5567ae779aa02d69fa12dc31e7ae18
Leonardo Setubal <setubal43@gmail.com>
2020-03-27 16:57:30 -0300

    feat: alteração dos stylus

--- src/pages/newinident/index.js
+++ src/pages/newinident/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiPower, FiTrash2 } from 'react-icons/fi'
+import { FiArrowUpLeft } from 'react-icons/fi'
 
 import logo from '../../assets/logo.svg'
 import { Link } from 'react-router-dom'
@@ -8,14 +8,28 @@ import './styles.css'
 const NewIncident = () => {
     return (
         <div className='incident-container'>
-            <header>
-            <img src={logo} alt="logo"></img>
-            <span>Bem Vinda APAD</span>
-            <Link className="button">Cadastrar Novo Caso</Link>
-            <button type='button'>
-                <FiPower size={18} color="#E02041"/>
-            </button>
-            </header>
+			<div className="content">
+                <section>
+                <img src={logo} alt="logo"></img>
+                <h1>Cadastrar novo Caso</h1>
+                <p>
+                Descreva o Caso detalhamente para 
+				encontrar um herói para resolver isso.
+                </p>
+                <Link  className="link-next-page" to="/profile">
+                        <FiArrowUpLeft size={16} color="#E02041" /> Não tenho cadastro?
+                </Link>
+                </section>
+				<form>
+                    <input type="text" placeholder="Titulo"/>
+                    <textarea type="text" placeholder="Descrição"/>
+                    <input type="text" placeholder="value"/>
+					<div className="btn-group">
+                    <button className="button">Cadastrar</button>
+                    <button className="button">Cancelar</button>
+                    </div>
+                </form>
+				</div>
         </div>
     );
 };
